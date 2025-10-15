24fa8913a9e4dc605829c323f86a713702a05f4d
rudybnb <rudybnbd@gmail.com>
2025-10-15 11:10:13 +0100

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,5 +1,5 @@
 // capacitor.config.ts
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.rudybnb.jobtracker',
