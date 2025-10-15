7ce0a9adf5fe29bc8b95842ecca7085567c1d682
rudybnb <rudybnbd@gmail.com>
2025-10-15 11:01:44 +0100

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,11 @@
-import { CapacitorConfig } from "@capacitor/cli";
+// capacitor.config.ts
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: "com.rudybnb.jobtracker",
-  appName: "Job Tracker",
- webDir: "dist/public"
-bundledWebRuntime: false
+  appId: 'com.rudybnb.jobtracker',
+  appName: 'Job Tracker',
+  webDir: 'dist/public',
+  bundledWebRuntime: false,
 };
 
 export default config;
